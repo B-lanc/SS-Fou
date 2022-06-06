51ed82cbfc33e9436999d7ce7a9aabd7abbb68de
b-lanc <jerry.tobing@binus.ac.id>
2022-06-06 22:20:31 +0700

    added dataset loading util

---
+++ utils/get_musdbhq.py
@@ -0,0 +1,35 @@
+import os
+import glob
+import numpy as np
+
+
+def load_dataset(path, inst):
+    """ """
+
+    tracks = glob.glob(os.path.join(path, "*"))
+    samples = list()
+
+    for track_folder in tracks:
+        track = dict()
+        for i in inst:
+            track[i] = os.path.join(track_folder, i + ".wav")
+        samples.append(track)
+
+    return samples
+
+
+def get_musdbhq(path, p_val=0.25):
+    """ """
+
+    instruments = ["bass", "drums", "mixture", "vocals", "other"]
+
+    trainval_set = load_dataset(os.path.join(path, "train"), instruments)
+    test_set = load_dataset(os.path.join(path, "test"), instruments)
+
+    # Shuffling the dataset and creating validation set
+    _middle = int(len(trainval_set) * p_val // 1)
+    np.random.shuffle(trainval_set)
+    train_set = trainval_set[_middle:]
+    val_set = trainval_set[:_middle]
+
+    return {"train": train_set, "val": val_set, "test": test_set}

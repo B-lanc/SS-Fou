acc75a6b8e8861ea33b4e5b126074178c92135dc
b-lanc <jerry.tobing@binus.ac.id>
2022-06-07 20:11:40 +0700

    added utility functions

---
+++ model/utils.py
@@ -0,0 +1,39 @@
+import os
+import torch
+
+
+def save_model(model, optimizer, state, path):
+    if isinstance(model, torch.nn.DataParallel):
+        model = model.module
+    if not os.path.exists(os.path.dirname(path)):
+        os.path.makedirs(os.path.dirname(path))
+    torch.save(
+        {
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "state": state,
+        },
+        path,
+    )
+
+
+def load_model(model, optimizer, path, cuda):
+    if isinstance(model, torch.nn.DataParallel):
+        model = model.module
+    checkpoint = torch.load(path, map_location=None if cuda else "cpu")
+    model.load_state_dict(checkpoint["model_state_dict"])
+    if optimizer is not None:
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
+    return checkpoint["state"]
+
+
+class DataParallel(torch.nn.DataParallel):
+    def __init__(self, module, device_ids=None, output_device=None, dim=0):
+        super(DataParallel, self).__init__(module, device_ids, output_device, dim)
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            return getattr(self.module, name)

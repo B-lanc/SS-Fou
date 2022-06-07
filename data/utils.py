68ab62d96885489bed53592fa2e96d7ac7383236
b-lanc <jerry.tobing@binus.ac.id>
2022-06-07 16:59:30 +0700

    added basic augment function

--- data/utils.py
+++ data/utils.py
@@ -8,3 +8,7 @@ def load_audio(path, sr, mono=False):
     if len(data.shape) == 1:
         data = data[np.newaxis, :]
     return data, sanity_sr
+
+
+def random_amplify(audio_data, p_min=0.0, p_max=1.0):
+    return np.clip(audio_data * np.random.uniform(p_min, p_max), -1.0, 1.0)
